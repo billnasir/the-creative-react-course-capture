05c42f5a7457549ff42d91aab994220e8f6e18ea
billnasir <billnasir@gmail.com>
2021-07-08 18:21:01 -0400

    2.About Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
  import React from "react";
+ import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-    <h1>Flim</h1>
-    </div>
+    <AboutUs/>
+     </div>
   );
 }
 
